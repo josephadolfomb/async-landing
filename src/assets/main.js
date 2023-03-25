fe0815709874524485c66c72033e1d280f635c48
josephadolfomb <josephmobu@gmail.com>
2023-03-25 17:36:29 -0500

    se agregó link para imagen del video

--- src/assets/main.js
+++ src/assets/main.js
@@ -21,6 +21,7 @@ async function fetchData(urlApi) {
         const videos = await fetchData(API);
         let view = `
         ${videos.items.map(video => `
+            <ahref="https://youtube.com/watch?v=${video.id.videoId}"target="_blank">
             <div class="group relative">
             <div
                 class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
